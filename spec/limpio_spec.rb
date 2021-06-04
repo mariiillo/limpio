4c6083646ceb59ea9f6087b18098cdfdc8a86bee
Mario Viapiano <marioeviapiano@gmail.com>
2021-06-04 16:59:00 +0200

    add specs

--- spec/limpio_spec.rb
+++ spec/limpio_spec.rb
@@ -6,43 +6,83 @@ RSpec.describe Limpio do
     expect(Limpio::VERSION).not_to be nil
   end
 
-  let(:hash) do
-    {
-      key1: "value1",
-      key2: "",
-      key3: {
-        key4: "value4",
-        key5: "",
-        key6: {
-          key7: {}
-        }
-      },
-      key8: {},
-      key9: nil,
-      key10: [],
-      key11: [nil, 1.23],
-      key12: [nil, 123, false, true],
-      key13: true,
-      key14: false
-    }
-  end
-
-  let(:output) do
-    {
-      key1: "value1",
-      key3: {
-        key4: "value4"
-      },
-      key11: [1.23],
-      key12: [123, false, true],
-      key13: true,
-      key14: false
-    }
-  end
-
   context "for Hashes" do
+    let(:hash) do
+      {
+        key1: "value1",
+        key2: "",
+        key3: {
+          key4: "value4",
+          key5: "",
+          key6: {
+            key7: {}
+          }
+        },
+        key8: {},
+        key9: nil,
+        key10: [],
+        key11: [nil, 1.23],
+        key12: [nil, 123, false, true],
+        key13: true,
+        key14: false
+      }
+    end
+
+    let(:output) do
+      {
+        key1: "value1",
+        key3: {
+          key4: "value4"
+        },
+        key11: [1.23],
+        key12: [123, false, true],
+        key13: true,
+        key14: false
+      }
+    end
+
     it "removes recursively keys with nil & empty values" do
       expect(hash.deep_reject_blank!).to eq(output)
+    end
+  end
+
+  context "for Arrays" do
+    let(:array) do
+      [
+        "value1",
+        "",
+        {
+          key4: "value4",
+          key5: "",
+          key6: {
+            key7: {}
+          }
+        },
+        {},
+        nil,
+        [],
+        [nil, 1.23],
+        [nil, 123, false, true],
+        true,
+        false
+      ]
+    end
+
+    let(:output) do
+      [
+        "value1",
+        {
+          key4: "value4"
+        },
+        [1.23],
+        [123, false, true],
+        true,
+        false
+      ]
+    end
+
+    it "removes recursively nil & empty elements" do
+      expect(array.deep_reject_blank!).to eq(output)
     end
   end
 end
